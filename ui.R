53af66c51fbe4cb4fc925c38252410be22eff16c
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-03-12 16:41:10 +0000

    plots, layout, style

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 #UI
 navbarPage(
-  title = "Media response to coronavirus",
+  title = "Media response to coronavirus - DRAFT - NO REAL DATA USED",
   windowTitle = "Media response to coronavirus", #title for browser tab
   header = tags$head(includeCSS("www/styles.css")),
   tabPanel(
@@ -20,24 +20,43 @@ navbarPage(
     mainPanel(width = 12,
     column(4, 
            plot_box("Visits to selected media", "hits_media_plot"),
+           plot_box("Number of retweets/favourites of selected media", "retweets_plot"),
+           plot_box("Visits to coronavirus pages in HPS", "hits_hps_plot")),
+    column(4, 
            plot_box("Sentiment analysis, 0 negative, 1 positive", "sentiment_plot"),
-           plot_box("Most used words", "word_cloud_plot")),
+           plot_box("Positive cases of coronavirus", "coronacases_plot"),
+           plot_box("Calls to NHS 24", "nhs24_plot")),
     column(4, 
-           plot_box("Calls to NHS 24", "nhs24_plot"),
-           plot_box("A&E attendances", "aye_plot"),
-           plot_box("Positive cases of coronavirus", "coronacases_plot")),
-    column(4, 
-           plot_box("Visits to coronavirus pages in HPS", "hits_hps_plot"),
-           plot_box("Retweets/favourites of", "retweets_plot"),
-           plot_box("Deaths attributed to Coronavirus", "coronadeaths_plot"))
+           plot_box("Most used words", "word_cloud_plot"),
+           plot_box("Deaths attributed to coronavirus", "coronadeaths_plot"),
+           plot_box("A&E attendances", "aye_plot"))
     )# mainPanel bracket
   ), # tabpanel bracket
   tabPanel(
-    title = "Correlations", icon = icon("chart-line")
-
+    title = "Correlations", icon = icon("chart-line"),
+    wellPanel(
+      column(6,
+             selectInput("measure1", label = "Step 1 - Select a measure",
+                         choices = measure_list, selected = "Visits to selected media")),
+      column(6,
+             selectInput("measure2", label = "Step 2 - Select another measure",
+                         choices = measure_list, selected = "Positive cases of coronavirus"))
+    ), #wellPanel bracket
+    mainPanel(width = 12,
+              p("This will show the relationship between some of the measures available."),
+              p("Here some results of the modelling between both measures could
+                be shown (ARIMA?)"),
+              plotlyOutput("correlation_plot")
+    )# mainPanel bracket
   ), # tabpanel bracket
   tabPanel(
-    title = "Data", icon = icon("table")
-
+    title = "Data", icon = icon("table"),
+    p("This tab could include the data used in the app. And potentially could have
+      the reactions to individual articles and tweets"),
+    downloadButton('download_table_csv', 'Download data'),
+    mainPanel(width = 12,
+              DT::dataTableOutput("table_filtered"))
   ) # tabpanel bracket
 ) # navbar bracket
+
+##END
